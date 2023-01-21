d5a2b2a53299667d4e22458edbddd455d2ca9b78
Nimesh Thapa Magar <nimesh.thapa.505@gmail.com>
2023-01-21 13:30:19 +0545

    Minor

--- lib/model/items.dart
+++ lib/model/items.dart
@@ -23,21 +23,21 @@ List<Item> all = [
       description: "This is the description of Item 1"),
   Item(
       name: "Item 2",
-      count: 0,
+      count: 10,
       url:
           "https://www.nepalisansar.com/wp-content/uploads/2018/04/Allo-Nepali-Handicrafts-300x225.jpg",
       price: 2000,
       description: "This is the description of Item 2"),
   Item(
       name: "Item 3",
-      count: 0,
+      count: 10,
       url:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1l2RwAGjDBCsiU1_CIjYu1ITyB3sgDGkl1T0fiqIABpUb-Dpms2b0dVnZDQPPQd9_igU&usqp=CAU",
       price: 1200,
       description: "This is the description of Item 3"),
   Item(
       name: "Item 4",
-      count: 0,
+      count: 10,
       url:
           "https://i0.wp.com/handicraftsinnepal.com/wp-content/uploads/2021/09/basketry-novelty-weaving-nepal.jpg?resize=534%2C356&ssl=1",
       price: 2200,
